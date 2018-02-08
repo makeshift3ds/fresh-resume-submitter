9b4c7bf587ac3104a9e88684c9d872b803db85dd
Ken Elliott <makeshift3ds@gmail.com>
2018-02-07 23:31:02 -0500

    add commander and sanity check

---
+++ index.js
@@ -0,0 +1,11 @@
+const program = require('commander')
+
+program
+  .version('0.0.1-alpha')
+  .option('-p, --path [str]', 'path to the json configuration')
+  .parse(process.argv)
+
+// Program Execution
+if (program.path) console.log(`path is set to ${program.path}`)
+
+// console.log(program);
